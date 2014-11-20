67da814c37692b415eeef863a22cc5a5623592df
Josh Rucker <josh.rucker@inin.com>
2014-11-19 22:47:50 -0800

    update sass files to partials, add styles for svg elements

--- wp-content/themes/markupshop/home.php
+++ wp-content/themes/markupshop/home.php
@@ -1,78 +1,105 @@
 <?php get_header(); ?>
 
+<?php
+    // load icons
+    get_sidebar('svg');
+?>
+
+
+
 <section id="intro">
-	
+
 	<h1 class="page-title">MarkupShop</h1>
 	<h2>A small development agency focused on helping people build the things they need.</h2>
-	
+
 </section>
 
 <section id="about">
-	
+
 	<h3 class="section-header">About Us</h3>
-	
+
 	<div class="about-text">
 		<p>Organizations and their clients are desperate for reliable development when they need it &mdash; and we're here to help.</p>
 		<p>We believe that well-crafted development is necessary to produce quality products. We're obsessive about delivering high-quality code and doing it on time.</p>
 		<p>We're not interested in being the biggest agency around &mdash; just a valuable partner for the folks we work with.</p>
 	</div>
-	
+
 </section><!--#about-->
 
 
 
 <section id="bios">
-	
+
 	<dl class="mobley">
 		<dt class="name">Josh Mobley</dt>
 		<dd class="job-title">Managing Partner</dd>
 		<dd class="skill">Front-end Development</dd>
 		<dd class="skill">User Experience Design</dd>
 	</dl>
-	
+
 	<dl class="rucker">
 		<dt class="name">Josh Rucker</dt>
 		<dd class="job-title">Partner</dd>
 		<dd class="skill">Full-stack Development</dd>
 		<dd class="skill">Content Direction</dd>
 	</dl>
-	
+
 </section>
-	
+
 <section id="services">
-	
+
 	<h3 class="section-header">Services</h3>
 
 	<dl class="development">
-		<dt>Front-End Development</dt>
+		<dt>
+            <span class="icon">
+                <svg viewBox="0 0 50 50" class="shape-tab">
+                    <use xlink:href="#icon-development"></use>
+                </svg>
+            </span>
+            Front-End Development
+        </dt>
 		<dd>Our bread and butter is custom built sites that convert on your goals.</dd>
 	</dl>
-	
+
 	<dl class="customization">
-		<dt>CMS Customization</dt>
+		<dt>
+            <span class="icon">
+                <svg viewBox="-1 0 25 28" class="shape-tab">
+                    <use xlink:href="#icon-customization"></use>
+                </svg>
+            </span>
+            CMS Customization
+        </dt>
 		<dd>Have a CMS that just needs a little work? We're here for you, too.</dd>
 	</dl>
-	
+
 	<dl class="support">
-		<dt>Ongoing Support</dt>
+		<dt>
+            <span class="icon">
+                <svg viewBox="0 0 15 15" class="shape-tab">
+                    <use xlink:href="#icon-support"></use>
+                </svg>
+            </span>
+            Ongoing Support
+        </dt>
 		<dd>We can also be your retainer-free support team.</dd>
 	</dl>
-	
+
 </section><!--#services-->
 
 <section id="contact">
-	
+
 	<div class="contact-text">
-	
+
 		<h3 class="section-header">Get In Touch</h3>
-	
+
 		<p>If you've got a project ready to start, or are just looking for someone to be your "go-to" developers in the future, we'd love to hear from you. We love talking with others about their projects and helping folks map out the way forward.</p>
-		
+
 	</div>
-	
+
 	<?php echo do_shortcode('[contact-form-7 id="7" title="Homepage Contact Form"]'); ?>
-	
+
 </section>
 
 <?php get_footer(); ?>
-	
