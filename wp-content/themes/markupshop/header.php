38e14ddba08f2ce454e6006933b4b58d5b9042a2
Josh Mobley <josh@Joshs-MacBook-Pro-2.local>
2014-12-04 22:07:50 -0500

    adding fav icon

--- wp-content/themes/markupshop/header.php
+++ wp-content/themes/markupshop/header.php
@@ -9,6 +9,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
     	
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/stylesheets/screen.css">
+		<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.png">
 		<!--[if lt IE 9]>
 		<script src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.js"></script>
 		<![endif]-->
