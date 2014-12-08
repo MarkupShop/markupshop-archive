aaf120ab27c20b3c8d72a7aad004827fc6725b9e
Josh Mobley <josh@Joshs-MacBook-Pro-2.local>
2014-12-08 14:38:49 -0500

    Adding title/description

--- wp-content/themes/markupshop/header.php
+++ wp-content/themes/markupshop/header.php
@@ -4,8 +4,8 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title></title>
-		<meta name="description" content="">
+		<title>MarkupShop</title>
+		<meta name="description" content="A small development agency focused on helping people build the things they need.">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
     	
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/stylesheets/screen.css">
