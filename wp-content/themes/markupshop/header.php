3571456fcd9fad266b96a11c20058a7370f3f081
Josh Rucker <ifiamblue@gmail.com>
2014-12-19 14:26:17 -0500

    Add og/twitter meta tags.

--- wp-content/themes/markupshop/header.php
+++ wp-content/themes/markupshop/header.php
@@ -8,13 +8,22 @@
 		<meta name="description" content="A small development agency focused on helping people build the things they need.">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
     	
+		<meta property="og:title" content="MarkupShop" />
+		<meta property="og:url" content="http://www.markupshop.com/" />
+		<meta property="og:description" content="A small development agency focused on helping people build the things they need." />
+		<meta property="og:type" content="website" />
+		<meta property="og:site_name" content="MarkupShop"/>
+		<meta name="twitter:card" content="summary" />
+		<meta name="twitter:site" content="@markupshop" />
+		<meta name="twitter:creator" content="@markupshop" />
+
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/stylesheets/screen.css">
 		<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.png">
 		
 		<!--[if lt IE 9]>
 		<script src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.js"></script>
 		<![endif]-->
-		
+
 		<script src="//use.typekit.net/fnj7kze.js"></script>
 		<script>try{Typekit.load();}catch(e){}</script>
 	</head>
